db3812bd6ba70a4a21612222a38f9b6646b6ceb9
Gabriel Cabral <gabrielcabralbfr@gmail.com>
2021-04-21 00:54:28 -0300

    fix mock repo

--- src/repositories/mock.repository.ts
+++ src/repositories/mock.repository.ts
@@ -1,13 +1,11 @@
 import { User } from '../entities/User'
 import { IRepository } from './interfaces/IRepository'
 import bcrypt from 'bcrypt'
-
 export class fakeRepository implements IRepository<any> {
 
     _db: Array<any> = []
     constructor() {
     }
-
 
     async save(user: User): Promise<any> {
         user.password = bcrypt.hashSync(user.password, 10);
@@ -15,15 +13,16 @@ export class fakeRepository implements IRepository<any> {
         return user
     }
 
-    async findAll(): Promise<User[]> {
+    async findAll(): Promise<any[]> {
         return this._db
     }
 
     async findByEmail(email: string): Promise<any> {
-        return this._db.filter(user => user.email === email)
+        const [item] = this._db.filter(user => user.email === email)
+        return item
     }
 
-    async login(loginData: User): Promise<User> {
+    async login(loginData: User): Promise<any> {
         return this.findByEmail(loginData.email)
     }
 }
