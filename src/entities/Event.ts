1c94892cddb4bf2ad55cb7f0434fe1ce9e4c4ce4
Gabriel Cabral <gabrielcabralbfr@gmail.com>
2021-04-21 03:05:58 -0300

    event model

---
+++ src/entities/Event.ts
@@ -0,0 +1,21 @@
+import { v4 as uuidv4 } from 'uuid';
+
+
+export class Event {
+    public readonly _id: string
+    public event_maker_user_id: number
+    public condo_id: number
+    public evemt_datetime: Date
+    public expectedAttendees: number
+    public event_category: string
+    /**
+     *
+     */
+    constructor(props: Omit<Event, '_id'>, id?: string) {
+        Object.assign(this, props)
+
+        if (!id) {
+            this._id = uuidv4()
+        }
+    }
+}
